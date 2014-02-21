430e129c09f5b409c43f81e129d51073723efbfb
cyclon1978 <cyclon1978@web.de>
2014-02-21 15:58:02 +0100

    more undervolting and the device will produce energy... but it works.

--- alps/mediatek/platform/mt6577/kernel/core/include/voltages.h
+++ alps/mediatek/platform/mt6577/kernel/core/include/voltages.h
@@ -43,11 +43,19 @@
 #define VOLTAGE_2_UV_3 (0x10)
 #define VOLTAGE_3_UV_3 (0x13)
 
+// not working -> freeze
 #define VOLTAGE_S_UV_4 (0x06)
 #define VOLTAGE_0_UV_4 (0x06)
 #define VOLTAGE_1_UV_4 (0x0F)
 #define VOLTAGE_2_UV_4 (0x0F)
 #define VOLTAGE_3_UV_4 (0x0F)
+
+// next try: sleep and 1GHz
+#define VOLTAGE_S_UV_5 (0x05)
+#define VOLTAGE_0_UV_5 (0x05)
+#define VOLTAGE_1_UV_5 (0x0D)
+#define VOLTAGE_2_UV_5 (0x10)
+#define VOLTAGE_3_UV_5 (0x12)
 
 #define VOLTAGE_S_DEF (0x08)
 #define VOLTAGE_0_DEF (0x08)
@@ -63,11 +71,11 @@
 // not working minimum for VOLTAGE_3 (max freq) is 0x0F -> freeze
 // working: 0x13
 
-#define VOLTAGE_S VOLTAGE_S_UV_4
-#define VOLTAGE_0 VOLTAGE_0_UV_4
-#define VOLTAGE_1 VOLTAGE_1_UV_4
-#define VOLTAGE_2 VOLTAGE_2_UV_4
-#define VOLTAGE_3 VOLTAGE_3_UV_4
+#define VOLTAGE_S VOLTAGE_S_UV_5
+#define VOLTAGE_0 VOLTAGE_0_UV_5
+#define VOLTAGE_1 VOLTAGE_1_UV_5
+#define VOLTAGE_2 VOLTAGE_2_UV_5
+#define VOLTAGE_3 VOLTAGE_3_UV_5
 
 #endif // __VOLTAGES_H__
 
