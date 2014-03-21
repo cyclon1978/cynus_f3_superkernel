71449092556090519d95d50c48276fcf21fd4f1a
cyclon1978 <cyclon1978@web.de>
2014-03-21 16:46:42 +0100

    only comments, no functionality

--- alps/mediatek/platform/mt6577/kernel/core/include/mach/mt_cpufreq.h
+++ alps/mediatek/platform/mt6577/kernel/core/include/mach/mt_cpufreq.h
@@ -1,6 +1,11 @@
-#ifndef _MT6577_CPUFREQ_H
-#define _MT6577_CPUFREQ_H
+/*
+ * BindassBoost Mtk6577 Smart Overclocking Interface 
+ *
+ *  Copyright (C) 2014 BindassDost.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
 
-extern void BBoost_Overclcock_manage(unsigned int overclock_freq);
-
-#endif
+extern void BBoost_Overclcock_manage(unsigned int overclock_status);
