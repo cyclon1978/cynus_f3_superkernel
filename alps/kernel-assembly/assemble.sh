197f0d6190dee19a69d3df624bbb8dc0730e0649
cyclon1978 <cyclon1978@web.de>
2014-06-30 12:35:56 +0200

    cleanup; better scripts

--- alps/kernel-assembly/assemble.sh
+++ alps/kernel-assembly/assemble.sh
@@ -1,13 +1,27 @@
+TOOLCHAIN=$(pwd)/../../toolchain/arm-cortex_a9-linux-gnueabihf-linaro_4.9.1-2014.05/bin
+
+export PATH=$TOOLCHAIN:$PATH
+export ARCH=arm
+export CROSS_COMPILE=arm-eabi-
+
+# cleanup old files
 rm ./boot.img-kernel.img
 rm ./system/lib/modules/*.ko
+rm ./f3_testkernel.zip
+
 cp ../kernel/out/kernel_s7511.bin ./boot.img-kernel.img
 cp ../kernel/out/mediatek/platform/mt6577/kernel/drivers/m4u/m4u.ko ./system/lib/modules/m4u.ko
 cp ../kernel/out/drivers/staging/zram/zram.ko ./system/lib/modules/zram.ko
 # cp ../kernel/out/lib/lzo/lzo_compress.ko ./system/lib/modules/lzo_compress.ko
 # cp ../kernel/out/lib/lzo/lzo_decompress.ko ./system/lib/modules/lzo_decompress.ko
 
+# strip modules
+echo "**** Patching all built modules (.ko) in /build_result/modules/ ****"
+find ./system/lib/modules/ -type f -name '*.ko' | xargs -n 1 $TOOLCHAIN/arm-eabi-strip --strip-unneeded
+
 # fix permissions
 chmod 755 ./boot.img-ramdisk/ -R
 
 ~/projects/tools/mtk-tools/repack-MT65xx.pl -boot boot.img-kernel.img boot.img-ramdisk boot.img
+cp ./f3_testkernel_template.zip ./f3_testkernel.zip
 zip -u -r ./f3_testkernel.zip ./boot.img system
