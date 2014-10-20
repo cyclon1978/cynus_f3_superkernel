4a22e6881f2a0fa6f578a27a8cdec357cde6ad37
Cyclon1978 <cyclon1978@web.de>
2014-10-20 13:49:24 +0200

    p2

--- alps/kernel-assembly/copy.sh
+++ alps/kernel-assembly/copy.sh
@@ -13,7 +13,7 @@ echo $INCREMENT > current.ver
 
 echo "Using version $INCREMENT ..."
 
-adb push ./f3_testkernel_v41.zip /sdcard/f3_kernel_6.$INCREMENT-4.1.zip
-adb push ./f3_testkernel_v42.zip /sdcard/f3_kernel_6.$INCREMENT-4.2.zip
+adb push ./f3_testkernel_v41.zip /sdcard/AAA/f3_kernel_6.$INCREMENT-4.1.zip
+# adb push ./f3_testkernel_v42.zip /sdcard/f3_kernel_6.$INCREMENT-4.2.zip
 
 echo "Done."
